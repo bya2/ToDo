543072f4085b1c9f5cb817ac275651ed41354e81
bya2 <byaa1972@gmail.com>
2021-08-10 23:03:57 +0900

    체육복: 비효율적이나 find 메서드를 사용했지만 1문제가 틀림

--- WorkoutClothes.js
+++ WorkoutClothes.js
@@ -1,40 +1,37 @@
 const solution = (n, lost, reserve) => {
   // 여벌의 체육복을 가지고 있었으나 도난당한 학생을 구분
   // 체육복이 2개인 학생들
-  const realReserve = reserve.filter(e => !lost.includes(e));
+  let realReserve = reserve.filter(e => !lost.includes(e));
 
   // 도난당한 학생 중 여벌의 체육복을 가지고 있어서 일단 체육복을 가지고 있는 학생들을 구분
   // 체육복을 가지고 있지 않은 학생들
   const noClothes = lost.filter(e => !reserve.includes(e));
 
-  // 전체 학생의 수에서 체육복을 가지고 있지 않은 학생들 제외
-  let answer = n - noClothes.length;
-
-  let i = n;
-  while (i--) {
-    const no = noClothes[0];
-    const r = realReserve[0];
-    if (!no || !r) break;
-
-    // 빌려주는 학생의 체격이 1보다 작을 때
-    if (no - r > 1) realReserve.shift(); 
-
-    // 빌려주는 학생의 체격이 1보다 클 때
-    if (no - r < -1) noClothes.shift();
-
-    // 둘의 체격차가 1일 때
-    if (Math.abs(no - r) === 1) {
-      noClothes.shift();
-      realReserve.shift();
-      answer++;
-    }
+  for (const no of noClothes) {
+    const isR = realReserve.find(r => Math.abs(no - r) === 1);
   }
+
+  // 전체 학생의 수에서 체육복을 가지고 있지 않은 학생들 제외
+  let skip = 0;
+
+  const answer = n - noClothes.filter(no => {
+    if (skip) return true;
+
+    // 체격차가 1인 학생 한 명
+    const isR = realReserve.find(r => Math.abs(no - r) === 1);
+    
+    // 체격차가 1이 아니라면 noClothes 배열에 남김
+    if (!isR) {
+      skip = 1;
+      return true;
+    }
+
+    // 체격차가 1인 학생이 있을 경우 여벌의 체육복을 가진 학생 목록에서 제외
+    realReserve = realReserve.filter(r => r !== isR);
+  }).length; // 끝으로 체육복을 가지고 있지 않은 학생의 수
 
   return answer;
 }
-
-// 여벌의 체육복을 가지고 있었으나 도난당한 학생을 구분
-// 체육복을 가지고 있지 않은 학생을 구분
 
 console.log(
   solution(5, [2, 4], [1, 3, 5]), // 5
