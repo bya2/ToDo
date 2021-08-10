d8ec2ec4d29eee455f0a3836b09e18c4b0d05325
bya2 <byaa1972@gmail.com>
2021-08-10 22:14:38 +0900

    체육복: shift가 for문에 영향을 줄 수 있다는 점을 알고 while문으로 수정했으나 점수는 높아졌지만 실패

--- Test.js
+++ Test.js
@@ -1,7 +1,6 @@
-const arr = [];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-if (arr) {
-  console.log(true);
-} else {
-  console.log(false);
+for (const x of arr) {
+  console.log(x); // 1 3 5 7 9
+  if (x % 2 != 0) arr.shift();
 }
--- WorkoutClothes.js
+++ WorkoutClothes.js
@@ -1,15 +1,31 @@
 const solution = (n, lost, reserve) => {
-  const realLost = lost.filter(x => !reserve.includes(x));
-  const realReserve = reserve.filter(x => !lost.includes(x));
+  // 여벌의 체육복을 가지고 있었으나 도난당한 학생을 구분
+  // 체육복이 2개인 학생들
+  const realReserve = reserve.filter(e => !lost.includes(e));
 
-  let answer = n - lost.length;
+  // 도난당한 학생 중 여벌의 체육복을 가지고 있어서 일단 체육복을 가지고 있는 학생들을 구분
+  // 체육복을 가지고 있지 않은 학생들
+  const noClothes = lost.filter(e => !reserve.includes(e));
 
-  for (const r of realReserve) {
-    const l = realLost[0];
-    if (!l) break;
+  // 전체 학생의 수에서 체육복을 가지고 있지 않은 학생들 제외
+  let answer = n - noClothes.length;
 
-    if (Math.abs(r - l) === 1) {
-      realLost.shift();
+  let i = n;
+  while (i--) {
+    const no = noClothes[0];
+    const r = realReserve[0];
+    if (!no || !r) break;
+
+    // 빌려주는 학생의 체격이 1보다 작을 때
+    if (no - r > 1) realReserve.shift(); 
+
+    // 빌려주는 학생의 체격이 1보다 클 때
+    if (no - r < -1) noClothes.shift();
+
+    // 둘의 체격차가 1일 때
+    if (Math.abs(no - r) === 1) {
+      noClothes.shift();
+      realReserve.shift();
       answer++;
     }
   }
@@ -17,8 +33,8 @@ const solution = (n, lost, reserve) => {
   return answer;
 }
 
-// lost의 요소 +-1을 reserve에서 찾는다.
-// reserver의 요소 +-1울 lost에서 찾는다.
+// 여벌의 체육복을 가지고 있었으나 도난당한 학생을 구분
+// 체육복을 가지고 있지 않은 학생을 구분
 
 console.log(
   solution(5, [2, 4], [1, 3, 5]), // 5
